c1bb241c3c10a8a34ef084d2c1a26e0bb024832d
francruz <kankuroamparo@gmail.com>
2015-03-13 22:12:02 +0100

    practica 1

---
+++ bottles.py
@@ -0,0 +1,7 @@
+
+a = 98
+
+while a!=-1:
+	print a+1, " bottles of beer on the wall, ", a+1, " bottles of beer. Take one down, pass it around, ", 
+	print a, " bottles of beer on the wall."
+	a = a-1
